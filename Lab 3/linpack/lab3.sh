200542faef2c4b56da4a52e6d4244d99002ca5a9
Nguyen Hong Hanh <41237821+hanhnguyen2685@users.noreply.github.com>
2022-01-07 21:35:03 +0300

    update result file

--- Lab 3/linpack/lab3.sh
+++ Lab 3/linpack/lab3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# echo "Let's start!" > out.txt
-# echo First stage!
-# for ((i = -20; i <= 19; i = $i + 3))
-# do
-# echo $i
-# echo $i >> out.txt
-# sudo nice -n $i ./linpack >> out.txt
-# done
-# echo First stage complete!
-
-for ((i = 2; i <= 32 ; i = $i *2))
+echo "Let's start!" > out.txt
+echo First stage!
+for ((i = -20; i <= 19; i = $i + 3))
 do
 echo $i
 echo $i >> out.txt
-sudo taskset -c 1-$i ./linpack >> out.txt
+sudo nice -n $i ./linpack >> out.txt
 done
+echo First stage complete!
+
+# for ((i = 2; i <= 32 ; i = $i *2))
+# do
+# echo $i
+# echo $i >> out.txt
+# sudo taskset -c 1-$i ./linpack >> out.txt
+# done
